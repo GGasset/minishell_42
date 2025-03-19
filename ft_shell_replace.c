14fcbab690e42dac2370a5a2d42b90aa0c6d2e02
German Gasset Marti <ggasset-@c2r3s4.42madrid.com>
2025-03-19 19:22:17 +0100

    improved comment

--- ft_shell_replace.c
+++ ft_shell_replace.c
@@ -6,13 +6,14 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:08:42 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/19 19:14:09 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:21:47 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_header.h"
 
 // Gets next dollarsign outside single quotes (')
+// If dollarsign is inside single quotes or followed by a non alpha numeric character is ignored
 static char	*get_next_dollar(char *s)
 {
 	size_t	i;
@@ -23,7 +24,7 @@ static char	*get_next_dollar(char *s)
 	while (s[i])
 	{
 		handle_quotes(s[i], &quotes);
-		if (quotes != '\'' && s[i] == '$' && ft_isalnum(s[i + 1]))
+		if (s[i] == '$' && quotes != '\'' && ft_isalnum(s[i + 1]))
 			return (s + i);
 		i++;
 	}

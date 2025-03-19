8df8711fd9a5296ec63e14ac31eb7411b43f5b48
German Gasset Marti <ggasset-@c2r15s6.42madrid.com>
2025-03-19 18:05:10 +0100

    added comment

--- text_utils.c
+++ text_utils.c
@@ -6,12 +6,17 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:59:31 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/19 18:00:20 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/19 18:03:44 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common_header.h"
 
+/*
+* - If quote is null or c is not a quote returns
+* - If *quote is 0 and c is a quote, sets *quote to c
+* - Else sets *quote to c 
+*/
 void	handle_quotes(char c, char *quote)
 {
 	if (!quote || !ft_isquotes(*quote))

fbb5246eb28164f2e53ef2aa0eec78ff7ac1671a
German Gasset Marti <ggasset-@c2r3s4.42madrid.com>
2025-03-19 19:19:26 +0100

    moved comment

--- parsing_header.h
+++ parsing_header.h
@@ -5,10 +5,10 @@
 # include "common_header.h"
 
 /*
- * 
+* - If quote is null or c is not a quote returns
+* - If *quote is 0 and c is a quote, sets *quote to c
+* - Else sets *quote to c 
 */
 void	handle_quotes(char c, char *quote);
-int		is_envvar_char(char c);
-
 
 #endif
--- text_utils.c
+++ text_utils.c
@@ -6,17 +6,12 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:59:31 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/19 18:03:44 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:18:28 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common_header.h"
 
-/*
-* - If quote is null or c is not a quote returns
-* - If *quote is 0 and c is a quote, sets *quote to c
-* - Else sets *quote to c 
-*/
 void	handle_quotes(char c, char *quote)
 {
 	if (!quote || !ft_isquotes(*quote))
@@ -25,9 +20,4 @@ void	handle_quotes(char c, char *quote)
 		*quote = 0;
 	else
 		*quote = c;
-}
-
-int	is_envvar_char(char c)
-{
-	return 0;
 }

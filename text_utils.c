c85f7c67bfa79ef33563447d2e49834b16706dc0
German Gasset Marti <ggasset-@c2r3s4.42madrid.com>
2025-03-24 14:44:31 +0100

    implemented definition for check_invalid_quotes

--- text_utils.c
+++ text_utils.c
@@ -6,7 +6,7 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:59:31 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/20 17:05:32 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/24 13:37:48 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,19 @@ void	handle_quotes(char c, char *quote)
 		*quote = 0;
 	else if (*quote == 0)
 		*quote = c;
+}
+
+int	check_invalid_quotes(char *s)
+{
+	char	out;
+	size_t	i;
+
+	out = 0;
+	i = 0;
+	while (s[i])
+	{
+		handle_quotes(s[i], &out);
+		i++;
+	}
+	return (out != 0);
 }

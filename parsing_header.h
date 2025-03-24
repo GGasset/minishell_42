0e55f9ed0a58deb4ae82aa5211242fc4724df7ce
German Gasset Marti <ggasset-@c2r7s3.42madrid.com>
2025-03-24 13:10:05 +0100

    added comment, added StdHeader

--- parsing_header.h
+++ parsing_header.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_header.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 13:09:19 by ggasset-          #+#    #+#             */
+/*   Updated: 2025/03/24 13:09:30 by ggasset-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef PARSING_HEADER_H
 # define PARSING_HEADER_H
@@ -10,6 +22,11 @@
 * - Else sets *quote to c 
 */
 void	handle_quotes(char c, char *quote);
+
+/*
+ * Returns malloc'ed char *
+ * Does not free s
+*/
 char	*ft_shell_replace(char *s, t_shell *shell);
 
 #endif

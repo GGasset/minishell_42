57ea6afd05ad297e24849920d0e3157518e78f78
German Gasset Marti <ggasset-@c2r6s4.42madrid.com>
2025-03-24 18:42:04 +0100

    added feature that removes white-space normalization inside quotes

--- ft_normalize_spaces.c
+++ ft_normalize_spaces.c
@@ -6,13 +6,13 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 14:49:09 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/24 15:33:18 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/24 18:41:10 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing_header.h"
 
-char	*trim_space(char *s, int free_s)
+static char	*trim_space(char *s, int free_s)
 {
 	char	*out;
 
@@ -24,34 +24,45 @@ char	*trim_space(char *s, int free_s)
 	return (out);
 }
 
-char	normalize_spaces(char c)
+static char	normalize_spaces(char c)
 {
 	if (ft_isspace(c))
 		c = ' ';
 	return (c);
 }
 
+static char	*append_normalized(char *s, char *out, size_t i, char quote)
+{
+	char	tmp[2];
+
+	tmp[0] = normalize_spaces(s[i]);
+	tmp[1] = 0;
+	if (!ft_isspace(s[i])
+		|| (i && ft_isspace(s[i]) && !ft_isspace(s[i - 1]))
+		|| quote)
+		out = ft_strjoin_free(out, tmp, TRUE, FALSE);
+	return (out);
+}
+
 char	*ft_normalize_spaces(char *s, int free_s)
 {
 	char	*out;
 	size_t	i;
-	char	tmp[2];
+	char	quote;
 
 	if (!s)
 		return (0);
 	out = ft_strdup("");
 	if (!out)
 		return (0);
-	tmp[1] = 0;
 	i = 0;
 	while (ft_isspace(s[i]))
 		i++;
+	quote = 0;
 	while (out && s[i])
 	{
-		tmp[0] = normalize_spaces(s[i]);
-		if (!ft_isspace(s[i])
-			|| (i && ft_isspace(s[i]) && !ft_isspace(s[i - 1])))
-			out = ft_strjoin_free(out, tmp, TRUE, FALSE);
+		handle_quotes(s[i], &quote);
+		out = append_normalized(s, out, i, quote);
 		i++;
 	}
 	if (free_s)

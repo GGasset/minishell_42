401ae5dcdba2250acb5d75740ce016ddc65b37f5
German Gasset Marti <ggasset-@c2r8s4.42madrid.com>
2025-03-27 12:49:51 +0100

    added definitions for is_word_delimiter and get_quote_at_point

--- text_utils.c
+++ text_utils.c
@@ -6,7 +6,7 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:59:31 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/03/24 18:56:16 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/03/27 12:48:25 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,28 @@ int	check_invalid_quotes(char *s)
 		i++;
 	}
 	return (out != 0);
+}
+
+int	is_word_delimiter(char c)
+{
+	if (ft_isspace(c))
+		return (TRUE);
+	if (c == '<' || c == '>' || c == '|')
+		return (TRUE);
+	return (FALSE);
+}
+
+char	get_quote_at_point(char *s, size_t point)
+{
+	char	quote;
+	size_t	i;
+
+	quote = 0;
+	i = 0;
+	while (s && s[i] && i < point)
+	{
+		handle_quotes(s[i], &quote);
+		i++;
+	}
+	return (quote);
 }

e2db1651be3135f324b5a0ae39580354fc048a7d
German Gasset Marti <ggasset-@c2r3s4.42madrid.com>
2025-04-02 16:31:33 +0200

    added is operator functions definitions

--- is_operator.c
+++ is_operator.c
@@ -6,7 +6,28 @@
 /*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 16:06:39 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/04/02 16:06:40 by ggasset-         ###   ########.fr       */
+/*   Updated: 2025/04/02 16:30:57 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "common_header.h"
+
+int	is_e_operator(int op)
+{
+	return (is_redirect_operator(op) || op == pipe_op);
+}
+
+int	is_redirect_operator(int op)
+{
+	return (is_input_e_operator(op) || is_output_e_operator(op));
+}
+
+int	is_input_e_operator(int op)
+{
+	return (op == stdin_redirect || op == stdin_delimiter);
+}
+
+int	is_output_e_operator(int op)
+{
+	return (op == stdout_redirect || op == stdout_append);
+}

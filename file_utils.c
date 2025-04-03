fc7db640041471405ffce180571fe6e60c9d6d67
German Gasset Marti <ggasset-@c2r7s3.42madrid.com>
2025-04-03 20:33:02 +0200

    added definition of create empty file

---
+++ file_utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggasset- <ggasset-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 18:45:57 by ggasset-          #+#    #+#             */
+/*   Updated: 2025/04/03 20:31:50 by ggasset-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "common_header.h"
+
+void	create_empty_file(char *path, t_shell *shell)
+{
+	int	fd;
+
+	if (!access(path, F_OK) && access(path, W_OK))
+	{
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+		return ;
+	}
+	fd = open(path, O_WRONLY | O_TRUNC | O_CREAT);
+	if (fd == -1)
+	{
+		ft_putstr_fd("ERROR\n", 2);
+		return ;
+	}
+	close(fd);
+}

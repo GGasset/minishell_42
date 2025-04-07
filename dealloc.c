ac213f7c5181d356b6d68a4d80496b017e8f8202
German Gasset Marti <ggasset-@c2r17s5.42madrid.com>
2025-04-07 15:47:36 +0200

    added definition for free_raw_line

---
+++ dealloc.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dealloc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggasset- <ggasset-@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-04-07 12:07:23 by ggasset-          #+#    #+#             */
+/*   Updated: 2025-04-07 12:07:23 by ggasset-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "common_header.h"
+
+void	free_raw_line(t_raw_line *line, int free_line)
+{
+	size_t      i;
+	t_raw_cmd   command;
+
+	if (!line)
+		return ;
+	i = 0;
+	while (line->raw_commands && i < line->len)
+	{
+		command = line->raw_commands[i];
+		free(command.file);
+		ft_free_splitted(command.argv);
+		free(command.input_redirect);
+		free(command.output_redirect);
+		ft_bzero(line->raw_commands + i, sizeof(t_raw_cmd));
+		i++;
+	}
+	free(line->raw_commands);
+	line->raw_commands = 0;
+	if (free_line)
+		free(line);
+}

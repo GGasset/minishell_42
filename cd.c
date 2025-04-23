7f9caa06445cb4998ee6b05718e33dab712a2d04
apaz <apaz-pri@student.42madrid.com>
2025-04-23 17:59:06 +0200

    kk

--- cd.c
+++ cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apaz-pri <apaz-pri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apaz-pri <apaz-pri@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 19:14:16 by apaz-pri          #+#    #+#             */
-/*   Updated: 2025/04/22 19:33:24 by apaz-pri         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:42:55 by apaz-pri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,32 @@
 	cd libft
 	cd ../algo
 	cd /tmp
-
-	1- Comprobar que exista
-	2- Si borran el directorio actual comprobar los casos en la bash
+	cd
+	
 */
 void	b_cd(t_exe exe, int j)
 {
-	char *new_pwd;
+    char	*path;
+    char	*new_pwd;
+    char	cwd[PATH_MAX];
 	
-	new_pwd = ft_strjoin("PWD=", exe.commands[j].argv[1]);
-	update_envp(exe.shell, new_pwd);
+	if (!exe.commands[j].argv[1])
+    {
+        path = get_envp("HOME", exe.shell->envp);
+        if (!path)
+        {
+            ft_putstr_fd("cd: HOME not set\n", 2);
+            return;
+        }
+    }
+    else
+        path = exe.commands[j].argv[1];
+	if (chdir(exe.commands[j].argv[1]) == -1)
+		printf("%s\n", strerror(errno));
+	if (getcwd(cwd, PATH_MAX))
+	{
+		new_pwd = ft_strjoin("PWD=", cwd);
+		update_pwd(exe.shell, new_pwd);
+		free(new_pwd);
+	}
 }
--- execution.c
+++ execution.c
@@ -6,10 +6,9 @@
 /*   By: apaz-pri <apaz-pri@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:57:44 by apaz-pri          #+#    #+#             */
-/*   Updated: 2025/04/21 16:33:43 by apaz-pri         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:54:08 by apaz-pri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "execution_header.h"
 
@@ -31,33 +30,80 @@ void	free2(char **str)
 */
 t_exe	prepare(t_raw_line rwcmd, t_shell *shell)
 {
-	t_exe   cmd;
+	t_exe	cmd;
 	int		i;
 
 	i = 0;
 	cmd.command_count = 0;
 	cmd.shell = shell;
 	cmd.commands = malloc(sizeof(t_cmd) * rwcmd.len);
-    if (!cmd.commands)
-    {
-        perror("Memory alloc Error");
-        exit(EXIT_FAILURE);
-    }
+	if (!cmd.commands)
+	{
+		perror("Memory alloc Error");
+		exit(EXIT_FAILURE);
+	}
 	while (i < rwcmd.len)
 	{
 		cmd.commands[i].path = get_from_path(rwcmd.raw_commands[i].file,
-		shell->envp);
+				shell->envp);
 		cmd.commands[i].argv = rwcmd.raw_commands[i].argv;
 		cmd.commands[i].input_fd = STDIN_FILENO;
 		cmd.commands[i].output_fd = STDOUT_FILENO;
 		if (rwcmd.raw_commands[i].input_redirect)
-			cmd.commands[i].input_fd = open(rwcmd.raw_commands[i].input_redirect->file, 
-			O_RDONLY);
+			cmd.commands[i].input_fd = open(rwcmd.raw_commands[i].input_redirect->file,
+					O_RDONLY);
 		if (rwcmd.raw_commands[i].output_redirect)
-			cmd.commands[i].output_fd = open(rwcmd.raw_commands[i].output_redirect->file, 
-			O_RDWR | O_TRUNC, 0644);
+			cmd.commands[i].output_fd = open(rwcmd.raw_commands[i].output_redirect->file,
+					O_RDWR | O_TRUNC, 0644);
 		cmd.command_count++;
 		i++;
 	}
 	return (cmd);
+}
+
+static int	is_builtin(char *cmd)
+{
+    if (!cmd)
+        return (0);
+    return (ft_strcmp(cmd, "cd") == 0 || ft_strcmp(cmd, "echo") == 0 ||
+            ft_strcmp(cmd, "pwd") == 0 || ft_strcmp(cmd, "export") == 0 ||
+            ft_strcmp(cmd, "unset") == 0 || ft_strcmp(cmd, "env") == 0 ||
+            ft_strcmp(cmd, "exit") == 0);
+}
+
+static void	execute_builtin(t_exe exe, int j)
+{
+    if (ft_strcmp(exe.commands[j].argv[0], "cd") == 0)
+        b_cd(exe, j);
+    else if (ft_strcmp(exe.commands[j].argv[0], "echo") == 0)
+        b_echo(exe.commands[j]);
+    else if (ft_strcmp(exe.commands[j].argv[0], "pwd") == 0)
+        b_pwd();
+    else if (ft_strcmp(exe.commands[j].argv[0], "export") == 0)
+        b_export(exe, j);
+    else if (ft_strcmp(exe.commands[j].argv[0], "unset") == 0)
+        b_unset(exe, j);
+    else if (ft_strcmp(exe.commands[j].argv[0], "env") == 0)
+        b_env(exe.shell);
+    else if (ft_strcmp(exe.commands[j].argv[0], "exit") == 0)
+        b_exit(exe, j);
+}
+
+void execute(t_exe exe)
+{
+	int	i;
+
+	i = 0;
+	while (i < exe.command_count)
+    {
+        if (is_builtin(exe.commands[i].argv[0]))
+            execute_builtin(exe, i);
+        else if (exe.commands[i].path)
+            execute_binary();
+		else
+		{
+			printf("%s commant not found\n", exe.commands[i].argv[0]);
+		}
+		i++;
+	}
 }

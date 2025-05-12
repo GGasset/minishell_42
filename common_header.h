974b1f3ccfa5dc1d6962b10c61abbf72d9515415
apaz <apaz-pri@student.42madrid.com>
2025-05-12 22:36:23 +0200

    remove common_header macro and makefile improvements

--- common_header.h
+++ common_header.h
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   common_header.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apaz-pri <apaz-pri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apaz-pri <apaz-pri@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:13:38 by ggasset-          #+#    #+#             */
-/*   Updated: 2025/05/12 18:02:28 by apaz-pri         ###   ########.fr       */
+/*   Updated: 2025/05/12 22:19:00 by apaz-pri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_HEADER_H
 # define COMMON_HEADER_H
+
+# ifndef HAVE_RL_CLEAR_HISTORY
+#  define rl_clear_history clear_history
+# endif
 
 # include "libft.h"
 # include <errno.h>

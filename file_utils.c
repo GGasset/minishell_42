8531e70fd7754b60e52b0f18cca420fbae3e01fb
GGasset <gassetgerman@gmail.com>
2025-05-15 17:58:32 +0200

    added auto-stop for outfile invalid permissions

--- file_utils.c
+++ file_utils.c
@@ -21,12 +21,14 @@ void	create_empty_file(char *path, t_shell *shell, int *err)
 	{
 		ft_putstr_fd(path, 2);
 		ft_putstr_fd(": Permission denied\n", 2);
+		*err = TRUE;
 		return ;
 	}
 	fd = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0666);
 	if (fd == -1)
 	{
 		ft_putstr_fd("ERROR\n", 2);
+		*err = TRUE;
 		return ;
 	}
 	close(fd);

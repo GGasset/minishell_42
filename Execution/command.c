e950a67813001e175a73eba2fb370907ec9c6982
Alejandro Paz Prieto <apaz-pri@c2r16s5.42madrid.com>
2025-05-19 18:58:24 +0200

    Last code

--- Execution/command.c
+++ Execution/command.c
@@ -6,7 +6,7 @@
 /*   By: apaz-pri <apaz-pri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 12:02:04 by apaz-pri          #+#    #+#             */
-/*   Updated: 2025/05/19 18:27:35 by apaz-pri         ###   ########.fr       */
+/*   Updated: 2025/05/19 18:55:35 by apaz-pri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 static void	e_child(t_cmd *cmd, t_exe exe, int **pipes, size_t idx)
 {
-	if (cmd->err)
-		exit(1);
 	if (cmd->input_fd < 0 || cmd->output_fd < 0)
 	{
 		perror("File descriptor");
@@ -62,12 +60,16 @@ static void	e_exec(t_exe exe, int **pipes)
 	i = -1;
 	while (++i < exe.command_count)
 	{
+		if (exe.commands[i].err)
+			continue ;
 		pid = fork();
-		exe.commands[i].pid = pid;
 		if (pid < 0)
 			perror("Fork:");
 		else if (pid == 0)
+		{
+			exe.commands[i].pid = pid;
 			e_child(&exe.commands[i], exe, pipes, i);
+		}
 		if (i > 0)
 			close(pipes[i - 1][0]);
 		if (i < exe.command_count - 1)
